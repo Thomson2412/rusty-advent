c5a759a5afcbdc3137964c633b69d4b9f55ba720
Thomson2412 <thomasm.w.fink@gmail.com>
2025-12-11 12:52:52 +0100

    Setup dev env

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Minimal Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            rust-analyzer
+            cargo-watch
+            cargo-nextest
+          ];
+
+          shellHook = ''
+            rustc --version
+            cargo --version
+          '';
+        };
+      }
+    );
+}
